c0ae0fb22a333be1330b77204a36f174664d928e
Abdulhadi Al Araj <130862794+AbdulhadiAlaraj@users.noreply.github.com>
2024-04-15 17:04:23 +0400

    Add files via upload

--- kitchenlang.rkt
+++ kitchenlang.rkt
@@ -3,17 +3,18 @@
 (require parser-tools/lex
          (prefix-in re- parser-tools/lex-sre)
          parser-tools/yacc)
+(require racket/hash)
 
 ; Define tokens for actions and ingredients
-(define-tokens kitchen-tokens (INGREDIENT))
+(define-tokens kitchen-tokens (INGREDIENT VAR))
 (define-empty-tokens kitchen-actions (CHOP MIX PEEL WHISK POUR EOF))
 (define-empty-tokens kitchen-conditional (TASTE-EQ TASTE-GT TASTE-LT))
 (define-empty-tokens kitchen-loop (BAKE))
 
 ; Lexer definitions
 (define-lex-abbrevs
-  (ingredient (re-+ (re-or (char-range "0" "9")))))
-
+  (letters (re-+ (re-range #\a #\z #\A #\Z)))
+  (digits (re-+ (re-range #\0 #\9))))
 
 (define kitchen-lexer
   (lexer
@@ -22,36 +23,43 @@
       (displayln (format "Next lexeme is ~a, Next token: INGREDIENT" lexeme))
       (token-INGREDIENT (string->number lexeme)))]
    ["chop" (begin
-            (displayln "Next lexeme is chop, Next token: KITCHEN-ACTION")
+            (displayln (format "Next lexeme is ~a, Next token: CHOP" lexeme))
             (token-CHOP))]
    ["mix" (begin
-           (displayln "Next lexeme is mix, Next token: KITCHEN-ACTION")
+           (displayln (format "Next lexeme is ~a, Next token: MIX" lexeme))
            (token-MIX))]
    ["bake" (begin
-            (displayln "Next lexeme is bake, Next token: KITCHEN-LOOP")
+            (displayln (format "Next lexeme is ~a, Next token: BAKE" lexeme))
             (token-BAKE))]
    ["peel" (begin
-            (displayln "Next lexeme is peel, Next token: KITCHEN-ACTION")
+            (displayln (format "Next lexeme is ~a, Next token: PEEL" lexeme))
             (token-PEEL))]
    ["whisk" (begin
-             (displayln "Next lexeme is whisk, Next token: KITCHEN-ACTION")
+             (displayln (format "Next lexeme is ~a, Next token: WHISK" lexeme))
              (token-WHISK))]
    ["taste-eq" (begin
-                 (displayln "Next lexeme is taste-eq, Next token: KITCHEN-CONDITIONAL")
+                 (displayln (format "Next lexeme is ~a, Next token: TASTE-EQ" lexeme))
                  (token-TASTE-EQ))]
    ["taste-gt" (begin
-                 (displayln "Next lexeme is taste-gt, Next token: KITCHEN-CONDITIONAL")
+                 (displayln (format "Next lexeme is ~a, Next token: TASTE-GT" lexeme))
                  (token-TASTE-GT))]
    ["taste-lt" (begin
-                 (displayln "Next lexeme is taste-lt, Next token: KITCHEN-CONDITIONAL")
+                 (displayln (format "Next lexeme is ~a, Next token: TASTE-LT" lexeme))
                  (token-TASTE-LT))]
    ["pour" (begin
-             (displayln "Next lexeme is pour, Next token: KITCHEN-ACTION")
+             (displayln (format "Next lexeme is ~a, Next token: POUR" lexeme))
              (token-POUR))]
+   [(re-+ (re-or (char-range #\a #\z) (char-range #\A #\Z)))
+    (begin
+      (displayln (format "Next lexeme is ~a, Next token: VAR" lexeme))
+      (token-VAR lexeme))]
    [whitespace (kitchen-lexer input-port)] ; Handles whitespace
    [(eof) (begin
            (displayln "Next lexeme is EOF, Next token: EOF")
            (token-EOF))])) ; End of file token
+
+
+
 ; Parser definitions
 (define-struct chop-exp (exp))
 (define-struct mix-exp (e1 e2))
@@ -62,7 +70,8 @@
 (define-struct taste-eq-exp (exp1 exp2 do-this else-do-this))
 (define-struct taste-gt-exp (exp threshold do-this else-do-this))
 (define-struct taste-lt-exp (exp threshold do-this else-do-this))
-(define-struct pour-exp (e1 e2))
+(define-struct var-exp (name))
+(define-struct pour-exp (var exp))
 
 (define (lex-this lexer input)
   (lambda ()
@@ -70,12 +79,16 @@
 
 (define kitchen-parser
   (parser
-   (start exp)
+   (start exp-list)
    (end EOF)
    (error void)
    (tokens kitchen-tokens kitchen-actions kitchen-conditional kitchen-loop)
    (grammar
+    (exp-list ((exp) (list $1))
+              ((exp-list exp) (append $1 (list $2))))
     (exp ((INGREDIENT) (ingredient-exp $1))
+         ((VAR) (var-exp $1))
+         ((POUR VAR exp) (pour-exp $2 $3))  ; Handle 'pour' followed by a variable and any expression
          ((CHOP exp) (chop-exp $2))
          ((MIX exp exp) (mix-exp $2 $3))
          ((BAKE CHOP exp INGREDIENT) (bake-exp 'chop $3 $4))
@@ -88,16 +101,26 @@
          ((TASTE-GT exp INGREDIENT exp exp) (taste-gt-exp $2 $3 $4 $5))
          ((TASTE-LT exp INGREDIENT exp exp) (taste-lt-exp $2 $3 $4 $5))))))
 
-; Adjust output-is to return the value after printing
+; Environment setup
+(define env (make-hash))
+
+; Adjust output to return the value after printing
 (define (output-is result)
   (displayln (format "output is: ~a" result))
   result)  ; Ensure it returns the result
+
+(define (eval-exp-list exp-list)
+  (map eval exp-list))
 
 ; Evaluation function modified to return values only
 (define (eval exp)
   (match exp
     ((ingredient-exp value) value)
     ((chop-exp e) (/ (eval e) 2))
+    ((var-exp var) (hash-ref env var 0)) ; Default to 0 if not found
+    ((pour-exp var exp) (let ([result (eval exp)])  ; Evaluate the expression and assign the result to the variable
+                          (hash-set! env var result)
+                          result))    
     ((mix-exp e1 e2) (+ (eval e1) (eval e2)))
     ((bake-exp operation exp times)
      (match operation
@@ -134,13 +157,17 @@
     ))
 
 ; Function to evaluate input and print final result
-(define (kitchen-code-evaluate input-string)
-  (let* ((input (open-input-string input-string))
-         (lexer-output (lex-this kitchen-lexer input))
+(define (kitchen-code-evaluate input-string-or-path)
+  (define input-port
+    (if (file-exists? input-string-or-path)
+        (open-input-file input-string-or-path)
+        (open-input-string input-string-or-path)))
+  (let* ((lexer-output (lex-this kitchen-lexer input-port))
          (parsed (kitchen-parser lexer-output))
-         (result (eval parsed)))
-    (output-is result)
-    (void)))  ; Print final result
+         (results (map eval parsed)))
+    (map output-is results)  ; Process and display each result
+    (close-input-port input-port)))
+
      ; Return void to suppress automatic output in interactive environments
 ; Example tests
 (kitchen-code-evaluate "chop 10")  ; Should return 5
@@ -168,3 +195,17 @@
 (kitchen-code-evaluate "taste-lt chop 10 3 peel 21 mix 1 2"); Should execute and return the result of mix 1 2 if false, else result of peel 21
 (displayln "-----------------------------------")
 (kitchen-code-evaluate "taste-lt bake mix 10 3 1 20 peel 21 mix 1 2"); Should execute and return the result of mix 1 2 if true, else result of peel 21
+(displayln "-----------------------------------")
+(kitchen-code-evaluate "test.kcl"); Reads the test.kcl file in the same directory
+(displayln "-----------------------------------")
+(kitchen-code-evaluate "pour x 20"); assigns 20 to x
+(displayln "-----------------------------------")
+(kitchen-code-evaluate "mix x x"); adds x to itself. Output is 40 here
+(displayln "-----------------------------------")
+(kitchen-code-evaluate "pour x 5"); reassigns 5 to x
+(displayln "-----------------------------------")
+(kitchen-code-evaluate "mix x x"); adds x to itself. output is 10
+(displayln "-----------------------------------")
+(kitchen-code-evaluate "pour y mix x x"); assign the output of mix x x to y
+(displayln "-----------------------------------")
+(kitchen-code-evaluate "whisk y y"); multiply y by itself. Ouput is 100 here
